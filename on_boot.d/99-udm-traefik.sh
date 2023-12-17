01e359128bde7280df12f70da0e1683d32eb7fd8
Bill ONeill <1393558+woneill@users.noreply.github.com>
2023-12-17 16:44:34 -0500

    Refactor script

--- on_boot.d/99-udm-traefik.sh
+++ on_boot.d/99-udm-traefik.sh
@@ -4,9 +4,10 @@
 # shellcheck source=../udm-traefik.env
 . /data/udm-traefik/udm-traefik.env
 
-if [ ! -f "${CRON_FILE}" ]; then
-	# Sleep for 5 minutes to avoid restarting
-	# services during system startup.
+traefik()
+{
 	sleep 300
 	sh "${UDM_TRAEFIK_PATH}"/udm-traefik.sh
-fi
+}
+
+traefik &
